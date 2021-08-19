4d40e3661ceae97d797a1dcce1605f98017c3429
Agorist-Action <agorist_action@protonmail.com>
2021-08-19 15:00:09 -0500

    Add Comment to IsExternalInit

--- src/OrdersCS/OrderRecord.cs
+++ src/OrdersCS/OrderRecord.cs
@@ -7,6 +7,5 @@ namespace TradingEngineServer.Orders
     /// <summary>
     /// Read-only representation of an order.
     /// </summary>
-    public record OrderRecord(long OrderId, uint Quantity, bool IsBuySide,
-        string Username, int SecurityId, uint TheoreticalQueuePosition);
+    public record OrderRecord(long OrderId, uint Quantity, bool IsBuySide, string Username, int SecurityId, uint TheoreticalQueuePosition);
 }
