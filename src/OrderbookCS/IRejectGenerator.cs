9a58497ae336af3a2edb6df7fb146bf3e4caac18
Agorist-Action <agorist_action@protonmail.com>
2021-08-21 11:13:26 -0500

    Create RejectGenerator

---
+++ src/OrderbookCS/IRejectGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TradingEngineServer.Orders;
+using TradingEngineServer.Rejects;
+
+namespace TradingEngineServer.Orderbook
+{
+    public interface IRejectGenerator
+    {
+        bool TryRejectNewOrder(Order order, IOrderEntryOrderbook orderbook, out Rejection rejection);
+        bool TryRejectModifyOrder(ModifyOrder modifyOrder, IOrderEntryOrderbook orderbook, out Rejection rejection);
+        bool TryRejectCancelOrder(CancelOrder cancelOrder, IOrderEntryOrderbook orderbook, out Rejection rejection);
+    }
+}
