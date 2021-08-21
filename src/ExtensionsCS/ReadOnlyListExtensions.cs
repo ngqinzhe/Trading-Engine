489d04c3c28e6753c3a90a1271c6bb33bc5bb7c6
Tzadiko <44016383+Tzadiko@users.noreply.github.com>
2021-08-20 19:30:51 -0500

    Add Trade Statistics Market Data (#18)
    * Add SecurityStatistics
    
    * Add TradeStatistics in OrderbookData
    
    * Simplify Multiple Set Methods in SecurityStatistics with AcceptPrice
    
    * Place ReadOnlyList Extension in List Namespace
    
    Co-authored-by: Agorist-Action <agorist_action@protonmail.com>

---
+++ src/ExtensionsCS/ReadOnlyListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TradingEngineServer.Extensions.List
+{
+    public static class ReadOnlyListExtensions
+    {
+        public static void ForEach<T>(this IReadOnlyList<T> list, Action<T> lambda)
+        {
+            foreach (var item in list)
+                lambda(item);
+        }
+    }
+}
