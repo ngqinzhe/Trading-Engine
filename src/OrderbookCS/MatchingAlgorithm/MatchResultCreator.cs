fb84c8bdeb17ab57cf6ba62500e3c6deb9d39688
Ng Qin Zhe <ngqinzhe2@gmail.com>
2021-09-21 16:12:56 +0800

    removed matchResult as a parameter

--- src/OrderbookCS/MatchingAlgorithm/MatchResultCreator.cs
+++ src/OrderbookCS/MatchingAlgorithm/MatchResultCreator.cs
@@ -7,8 +7,9 @@ namespace TradingEngineServer.Orderbook.MatchingAlgorithm
 {
     internal class MatchResultCreator
     {
-        public static MatchResult CreateMatchResult(MatchResult matchResult, TradeResult tradeResult, OrderbookEntry orderToMatchBid, OrderbookEntry orderToMatchAsk, DateTime eventTime)
+        public static MatchResult CreateMatchResult(TradeResult tradeResult, OrderbookEntry orderToMatchBid, OrderbookEntry orderToMatchAsk, DateTime eventTime)
         {
+            MatchResult matchResult = new MatchResult();
             matchResult.AddTradeResult(tradeResult);
             bool buySideIsAggressor = orderToMatchBid.CreationTime > orderToMatchAsk.CreationTime;
             Limit relevantOrderbookLimit = buySideIsAggressor ? orderToMatchAsk.ParentLimit : orderToMatchBid.ParentLimit;
